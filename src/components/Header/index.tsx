6ed19cccc029fada67b41a1e09e39ff0ea6efaaa
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-10-10 19:59:19 -0300

    app tarefas plus

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -44,3 +44,38 @@ export function Header() {
     </header>
   )
 }
+
+
+{/* <header className={styles.container}>
+  <section className={styles.content}>
+
+    <nav className={styles.nav}>
+
+      <Link href={'/'}>
+        <h1 className={styles.logo}>
+          Tarefas <span>Plus</span>
+        </h1>
+      </Link>
+
+      {session?.user && (
+        <Link href={'/dashboard'} className={styles.linkPainel}>
+          Meu painel
+        </Link>
+      )}
+
+    </nav>
+
+    {status === "loading" ? (
+      <></>
+    ) : session ? (
+      <button className={styles.loginButton} onClick={() => signOut()}>
+        {session?.user?.name}
+      </button>
+    ) : (
+      <button className={styles.loginButton} onClick={() => signIn('google')}>
+        Acessar
+      </button>
+    )}
+
+  </section>
+</header> */}
